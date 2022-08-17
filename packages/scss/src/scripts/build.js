585271f81fc4a16edac61d3ff01721a1608abab8
dang.pm.IT <longdieu12x@gmail.com>
2022-08-17 19:29:12 +0700

    Day two

---
+++ packages/scss/src/scripts/build.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+const path = require("path");
+
+const sass = require("node-sass");
+
+const getComponents = () => {
+	let allComponents = [];
+
+	const types = ["atoms", "molecules", "organisms"];
+
+	types.forEach((type) => {
+		const allFiles = fs.readdirSync(`src/${type}`).map((file) => {
+			return {
+				input: `src/${type}/${file}`,
+				output: `src/lib/${file.slice(0, -4)}css`,
+			};
+		});
+		allComponents = [...allComponents, ...allFiles];
+	});
+
+	return allComponents;
+};
+
+const compile = (pathname, fileName) => {
+	const result = sass.renderSync({
+		data: fs.readFileSync(pathname).toString(),
+		outputStyle: "expanded",
+		outFile: "global.css",
+		includePaths: [path.resolve("src")],
+	});
+
+	fs.writeFileSync(path.resolve(fileName), result.css.toString());
+};
+
+getComponents().forEach((component) => {
+	compile(component.input, component.output);
+});
