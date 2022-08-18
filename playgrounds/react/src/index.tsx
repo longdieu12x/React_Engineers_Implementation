c1b8eaab73a3dbd19e9a9f5bdb90c738d2578735
dang.pm.IT <longdieu12x@gmail.com>
2022-08-18 16:03:30 +0700

    add all

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+import { Color } from '@ds.e/react'
+import '@ds.e/scss/lib/Utilities.css'
+
+ReactDOM.render(
+  <Color hexCode="#73512d" height='xl' width='xl'></Color>,
+  document.querySelector('#root')
+)
